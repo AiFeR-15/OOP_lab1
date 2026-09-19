8a3c6d656d8fc1044502cfafa7def15610658bcb
AiFeR-15 <solonynko2020@gmail.com>
2026-09-19 21:33:17 +0300

    описав функциї, гловну функцию

--- lab1_1.c
+++ lab1_1.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
+
 typedef struct store s;
 struct store{
     double first;
     int second;
 };
 
-void input(s store,int n){
-    for(int i=0; i<n; i++){
-
-    }
+int check(double f, int s){
+    if(f>0 && s>0) return 1;
+    return 0;
 }
 
-void output(s store, int n){
-    for(int i=0; i<n; i++){
+void input(s *ob){
+    do{
+        printf("Enter price (real number) and quantity (integer): ");
+        scanf("%lf %d", &ob->first, &ob->second);
+        if(check(ob->first,ob->second)==0)
+        printf("Error! Values must be positive\n");
+    }while(check(ob->first,ob->second)==0);
+}
 
-    }
-
+void output(s ob){
+    printf("Product price: %.2lf\n", ob.first);
+    printf("Product quantity: %d\n", ob.second);
 } 
-
+double cost(s ob){
+    return (double) ob.first * ob.second;
+}
+double costTwo(s ob,s ob1){
+    return cost(ob) + cost(ob1);
+}
 
 int main(){
 
+
+    s ob1, ob2;
+    printf("--- Product 1 ---\n");
+    input(&ob1);
+    
+    printf("--- Product 2 ---\n");
+    input(&ob2);
+    
+    printf("\n--- Results ---\n");
+    output(ob1);
+    printf("Cost of the first: %.2lf\n\n", cost(ob1));
+
+    output(ob2);
+    printf("Cost of the second: %.2lf\n\n", cost(ob2));
+
+    printf("Total cost: %.2lf\n", costTwo(ob1, ob2));
 return 0;
 }
