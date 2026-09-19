afab1c5347a21eb743b511cd1c34862818a69d57
AiFeR-15 <solonynko2020@gmail.com>
2026-09-19 15:37:13 +0300

    опис функций

---
+++ lab1_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+typedef struct store s;
+struct store{
+    double first;
+    int second;
+};
+
+void input(s store,int n){
+    for(int i=0; i<n; i++){
+
+    }
+}
+
+void output(s store, int n){
+    for(int i=0; i<n; i++){
+
+    }
+
+} 
+
+
+int main(){
+
+return 0;
+}
